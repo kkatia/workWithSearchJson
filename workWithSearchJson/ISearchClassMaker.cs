99224cd6fde3313e6d38f396a3d64e3656d2d5c0
kkatia <komelina.katia@ya.ru>
2018-12-16 19:49:37 +0300

    commit

---
+++ workWithSearchJson/ISearchClassMaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workWithSearchJson
+{
+    interface ISearchClassMaker
+    {
+        string type { get; set; }
+        Instrumentation instrumentation { get; set; }
+        List<tagsEl> tags { get; set; }
+        ImageR image { get; set; }
+
+      
+
+    }
+     class Instrumentation { }
+    class ImageR { }
+    class tagsEl { }
+
+
+}
--- workWithSearchJson/Program.cs
+++ workWithSearchJson/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace workWithSearchJson
 {
+     
+    class human
+    {
+        public virtual string Said()
+        {
+            return "i am human";
+        }
+    }
+    class child : human
+    {
+        public override string Said()
+        {
+            return "i am child"; 
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            human item1 = new human();
+            human item2 = new child();
+           
+            Console.WriteLine(
+            item1.Said()+
+            "\n"+
+            item2.Said());
+            Console.ReadKey();
         }
     }
 }
